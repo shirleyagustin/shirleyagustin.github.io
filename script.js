8462298b5fad34af55ee25d15d822c4251a395ca
Shirley Agustin <shirley.agust03@gmail.com>
2024-11-14 12:19:57 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-// You can add any JavaScript you need here. For example:
-document.addEventListener('DOMContentLoaded', function () {
-    console.log('Document loaded and ready!');
+// Script for Scroll Highlighting
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".navbar nav a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop - 150;
+    if (scrollY >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.remove("active-link");
+    if (link.getAttribute("href").includes(current)) {
+      link.classList.add("active-link");
+    }
+  });
 });
 
